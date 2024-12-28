b63becdbe345fc20bc4a8b36917f390a20e823e7
Yuval-Betito <yuval36811@gmail.com>
2024-12-29 01:07:56 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import register, user_login, home  # Use the renamed user_login view
+from .views import register, user_login, home, create_customer  # Include create_customer view
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', user_login, name='login'),
     path('home/', home, name='home'),
+    path('customer/add/', create_customer, name='add_customer'),  # URL for adding a customer
 ]
